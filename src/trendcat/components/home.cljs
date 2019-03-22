fe5a3e610ee7540a12e67c7a5c0a7c0dc1d696a3
Ergenekon Yigit <yigitergenekon@gmail.com>
2019-03-22 10:10:25 +0300

    fix responsive layout issue

--- src/trendcat/components/home.cljs
+++ src/trendcat/components/home.cljs
@@ -30,39 +30,40 @@
                                  :data-size "large"
                                  :aria-label "Star ergenekonyigit/trendcat on GitHub"}                                
                "Github"]]]
-            [:div {:style {:display "flex"
-                           :flex-direction "row"
-                           :justify-content "space-between"}}
-             [:div
-              [:span.title "Trending repositories on GitHub"]
-              [:span {:style {:margin-left "10px"}}
-               (since-name)]]
-             [:div
-              [:div.select
-               [:select {:on-change #(do
-                                       (swap! app-state assoc :lang (-> % .-target .-value))
-                                       (get-repositories {:since (:since @app-state)
-                                                          :lang (:lang @app-state)}))}
-                (for [language (:languages @app-state)]
-                  ^{:key (last (last language))}
-                  [:option {:value (last (first language))}
-                   (last (last language))])]]
-              [:div.dropdown.is-hoverable {:style {:margin "0 5px"}}
-               [:div.dropdown-trigger
-                [:button {:class "button"
-                          :aria-haspopup "true"
-                          :aria-controls "dropdown-menu"
-                          :style {:width "110px"}}
-                 [:span (since-name)]]]
-               [:div#dropdown-menu {:class "dropdown-menu"
-                                    :role "menu"}
-                [:div.dropdown-content
-                 (for [item dropdown-items]
-                   ^{:key (first item)}
-                   [:a.dropdown-item {:on-click #(do
-                                                   (swap! app-state assoc :since (first item))
-                                                   (get-repositories {:since (:since @app-state)
-                                                                      :lang (:lang @app-state)}))}
-                    (last item)])]]]
-              ]]]]]]
-        [list-view]]])))
+            [:div ;; {:style {:display "flex"
+             ;;          :flex-direction "row"
+             ;;          :justify-content "space-between"}}
+             [:div.columns.is-desktop
+              [:div {:style {:margin-bottom "10px"}}
+               [:span.title "Trending repositories on GitHub"]
+               [:span {:style {:margin-left "10px"}}
+                (since-name)]]
+              [:div {:style {:margin-left "auto"}}
+               [:div.select
+                [:select {:on-change #(do
+                                        (swap! app-state assoc :lang (-> % .-target .-value))
+                                        (get-repositories {:since (:since @app-state)
+                                                           :lang (:lang @app-state)}))}
+                 (for [language (:languages @app-state)]
+                   ^{:key (last (last language))}
+                   [:option {:value (last (first language))}
+                    (last (last language))])]]
+               [:div.dropdown.is-hoverable {:style {:margin "0 5px"}}
+                [:div.dropdown-trigger
+                 [:button {:class "button"
+                           :aria-haspopup "true"
+                           :aria-controls "dropdown-menu"
+                           :style {:width "110px"}}
+                  [:span (since-name)]]]
+                [:div#dropdown-menu {:class "dropdown-menu"
+                                     :role "menu"}
+                 [:div.dropdown-content
+                  (for [item dropdown-items]
+                    ^{:key (first item)}
+                    [:a.dropdown-item {:on-click #(do
+                                                    (swap! app-state assoc :since (first item))
+                                                    (get-repositories {:since (:since @app-state)
+                                                                       :lang (:lang @app-state)}))}
+                     (last item)])]]]
+               ]]]]]]]
+         [list-view]]])))
